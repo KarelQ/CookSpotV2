f30eb2f537928fbcbfe253495f4b4cde165657b8
KarelQ <123106509+KarelQ@users.noreply.github.com>
2025-01-23 03:06:32 +0100

    added Categories component

---
+++ Backend/src/main/java/com/example/cookspot/entity/Category.java
@@ -0,0 +1,39 @@
+package com.example.cookspot.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "categories_id_gen")
+    @SequenceGenerator(name = "categories_id_gen", sequenceName = "categories_id_category_seq", allocationSize = 1)
+    @Column(name = "id_category", nullable = false)
+    private Integer id;
+
+    @Column(name = "category_name")
+    private String categoryName;
+
+    @Column(name = "category_desc")
+    private String categoryDesc;
+
+    @ManyToMany(mappedBy = "postCategoriesList")
+    Set<Post> postCategory;
+
+//    public Category getCategory() {
+//        return this;
+//    }
+
+//    @ManyToMany
+//    @JoinTable(name = "post_categories",
+//            joinColumns = @JoinColumn(name = "id_category"),
+//            inverseJoinColumns = @JoinColumn(name = "id_post"))
+//    private Set<Post> posts = new LinkedHashSet<>();
+
+}
