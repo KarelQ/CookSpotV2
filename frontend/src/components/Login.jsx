d4ea793b73db16857a2fec33d54cf3bac9dfe781
KarelQ <123106509+KarelQ@users.noreply.github.com>
2025-01-29 04:10:58 +0100

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -2,12 +2,15 @@ import React, {useEffect, useState} from "react";
 import style from "/src/css/login.module.css";
 import {login} from "../services/UserService.jsx";
 import {v4 as uuidv4} from "uuid";
+import {useNavigate} from "react-router-dom";
 
 const Login = () => {
-    const [messages, setMessages] = useState(["Example error message"]); // Przykładowe dane
+    const [messages, setMessages] = useState([""]); // Przykładowe dane
 
     const [email, setEmail] = useState([""]);
     const [password, setPassword] = useState("");
+
+    const navigate = useNavigate();
 
 
 
@@ -25,10 +28,25 @@ const Login = () => {
         login(user).then((response) => {
             console.log(response.data);
 
+            if(response.data !== ""){
+                sessionStorage.setItem("sessionUserId", response.data[0]);
+                sessionStorage.setItem("sessionUsername", response.data[1]);
+                sessionStorage.setItem("sessionIdRole", response.data[2]);
+
+                navigate(`/mainpage`);
+
+            } else {
+                setMessages(["Wrong email or password"]);
+            }
+            console.log(sessionStorage.getItem("sessionUserId"));
+
+
         });
     }
+
     return (
-        <div className={style["base-container-login"]}>
+
+        <div id={"login"} className={style["base-container-login"]}>
             <div className={style["login-container"]}>
                 <img src="/src/assets/logo_with_name.png" alt="logo alt"/>
 
