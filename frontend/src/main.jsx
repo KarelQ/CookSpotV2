cae82a5110ee7955524d7491dde924af68d492cb
KarelQ <123106509+KarelQ@users.noreply.github.com>
2025-01-29 04:11:08 +0100

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,13 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-// Dynamicznie ustaw id na div#root
-const rootElement = document.getElementById("root");
-if (window.location.pathname === "/login" || window.location.pathname === "/register") {
-    rootElement.setAttribute("class", "login-bimg");
-} else {
-    rootElement.setAttribute("class", "bimg base-container"); // domyślne
-}
 
 
 
